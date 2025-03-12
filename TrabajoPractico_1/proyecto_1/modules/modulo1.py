461d587aaccf2b815a197b31baa5b555ded896b4
agwursten <augustogw@gmail.com>
2025-03-12 12:05:12 -0300

    Funciones imprimir

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,9 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+def imprimir():
+    print("ñom ñom")
+    
+def imprimir2():
+    print("burguesa")
+    
